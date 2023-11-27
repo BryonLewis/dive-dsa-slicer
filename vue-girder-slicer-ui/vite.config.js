feb9e07247fbdc49bdf0189b1705f57dd7b3292c
Bryon Lewis <Bryon.Lewis@kitware.com>
2023-11-27 14:19:43 -0500

    adding types building

--- vue-girder-slicer-ui/vite.config.js
+++ vue-girder-slicer-ui/vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { resolve } from 'path'
+import dts from 'vite-plugin-dts'
 
 // https://vitejs.dev/config/
 export default defineConfig(() =>{
@@ -26,7 +27,7 @@ export default defineConfig(() =>{
         },
       },
     },
-    plugins: [vue()],
+    plugins: [vue(), dts({ rollupTypes: true })],
     server: {
       host: "0.0.0.0",
       port: 8080,
