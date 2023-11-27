d13734ef1ad411b158f1f0121e51965922a27bd3
Bryon Lewis <Bryon.Lewis@kitware.com>
2023-11-27 11:11:50 -0500

    update viteconfig for lib building

--- vue-girder-slicer-ui/vite.config.js
+++ vue-girder-slicer-ui/vite.config.js
@@ -1,9 +1,31 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig(() =>{
   return {
+    build: {
+      lib: {
+        // Could also be a dictionary or array of multiple entry points
+        entry: resolve(__dirname, './src/main.js'),
+        name: 'MyLib',
+        // the proper extensions will be added
+        fileName: 'my-lib',
+      },
+      rollupOptions: {
+        // make sure to externalize deps that shouldn't be bundled
+        // into your library
+        external: ['vue'],
+        output: {
+          // Provide global variables to use in the UMD build
+          // for externalized deps
+          globals: {
+            vue: 'Vue',
+          },
+        },
+      },
+    },
     plugins: [vue()],
     server: {
       host: "0.0.0.0",
